e723c844fca75bdbea165b17fb017aff9e5a3afb
Ky <ky.truong@digital.justice.gov.uk>
2024-09-06 14:19:03 +0100

    :rocket:Initial setup of cni-metrics helper

--- variables.tf
+++ variables.tf
@@ -2,7 +2,15 @@
 # Configuration #
 #################
 
-# Add module-specific variables here
+variable "cluster_domain_name" {
+  description = "The cluster domain used for externalDNS annotations and certmanager"
+}
+
+variable "eks_cluster_oidc_issuer_url" {
+  description = "If EKS variable is set to true this is going to be used when we create the IAM OIDC role"
+  type        = string
+  default     = ""
+}
 
 ########
 # Tags #
